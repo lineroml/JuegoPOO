3388dc755d20b79631267771d3aabc095b11049e
luisa <lineroml@uninorte.edu.co>
2020-03-18 22:45:24 -0500

    Primer commit

---
+++ src/juegopoo/JuegoPOO.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package juegopoo;
+
+/**
+ *
+ * @author luisa
+ */
+public class JuegoPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Holi, porfa cada que necesiten trabajar trabajen
+        sobre sus propias ramas (si no existe, creela).
+        En caso tal de dañar todo en la rama en la que 
+        esten (NUNCA TRABAJEN EN EL MASTER), creen otra
+        y vuelvan a iniciar su trabajo sobre esa rama.
+        En caso tal de terminar de trabajar lo que iban
+        a hacer por favor avisen en el grupo que harán
+        merge. No hagan cambios repentinos sin previo 
+        aviso ya que no se pueden revertir merges.
+        Les deseo un lindo semestre y que salga lo mejor
+        de esto.
+                        Felices Días de CoronaVirus.
+                            Saludos, Luisa.
+                                <3
+        */
+    }
+    
+}
