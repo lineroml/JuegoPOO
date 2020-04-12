0d9495a90519cb9e3cf233f4e44179b7d26da304
Luis Felipe Evilla <luisfelipe588@hotmail.com>
2020-04-11 21:27:40 -0500

    temporizador para controlar las actualizacion por segundo

--- juegoPOO/src/juegoPOO/juegoPOO.java
+++ juegoPOO/src/juegoPOO/juegoPOO.java
@@ -21,6 +21,9 @@ public class juegoPOO extends Canvas implements Runnable{
 
     private static final String NOMBRE = "Forgotten History";
 
+    private static int aps = 0; // actualizaciones por segundo
+    private static int fps = 0;  // cuadros por segundo
+    
     // con la palabra volatile esta variable no puede ser utilizada al mismo timepo por los dos hilos(thread)
     private volatile static boolean running = false;
     
@@ -64,12 +67,51 @@ public class juegoPOO extends Canvas implements Runnable{
         }
     }
     
+    private void actualizar() {
+        aps++;
+    }
     
+    private void dibujar() {
+        fps++;
+    }
+
+    
+    // Este metodo sera llamado cada vez que un nuevo hilo sea creado
     @Override
     public void run() {
-        // Este metodo sera llamado cada vez que un nuevo hilo sea creado
         while(running) {
+            final int NS_POR_SEGUNDO = 1000000000;
+            final byte APS_OBJETIVO= 60;
+            final double NS_POR_ACTUALIZACION = NS_POR_SEGUNDO / APS_OBJETIVO; // cada cuantos nanosegndos se hara una actualizacion
             
+            long actualizacionReferencia = System.nanoTime();
+            long contadorReferencia = System.nanoTime();
+            
+            double tiempoTranscurrido;
+            double delta = 0;
+            
+            while(running) {
+                final long inicioBucle = System.nanoTime();
+                
+                tiempoTranscurrido = inicioBucle - actualizacionReferencia;
+                actualizacionReferencia = inicioBucle;
+                
+                delta += tiempoTranscurrido / NS_POR_ACTUALIZACION;
+                
+                while (delta >= 1) {
+                    actualizar();
+                    delta--;
+                }
+                
+                dibujar();
+                
+                if (System.nanoTime() - contadorReferencia > NS_POR_SEGUNDO) {
+                    ventana.setTitle(NOMBRE + " || aps: " + aps + " || FPS: " + fps);
+                    aps = 0;
+                    fps = 0;
+                    contadorReferencia = System.nanoTime();
+                }
+            }
         }
     }
     
