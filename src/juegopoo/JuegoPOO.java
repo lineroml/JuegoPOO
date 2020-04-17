6f76906829fd6ed02bccdad890151ce36ea7fca8
Luis Felipe Evilla <luisfelipe588@hotmail.com>
2020-04-16 22:26:37 -0500

    arreglando problema de dos mains

--- src/juegopoo/JuegoPOO.java
+++ src/juegopoo/JuegoPOO.java
@@ -3,35 +3,116 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package juegopoo;
+package juegoPOO;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
 /**
  *
- * @author luisa
+ * @author luisf
  */
-public class JuegoPOO {
+public class juegoPOO extends Canvas implements Runnable{
 
-    /**
-     * @param args the command line arguments
-     */
+    private static final int ANCHO = 800;
+    private static final int ALTO = 600;
+
+    private static final String NOMBRE = "Forgotten History";
+
+    private static int aps = 0; // actualizaciones por segundo
+    private static int fps = 0;  // cuadros por segundo
+    
+    // con la palabra volatile esta variable no puede ser utilizada al mismo timepo por los dos hilos(thread)
+    private volatile static boolean running = false;
+    
+    private static JFrame ventana;
+    private static Thread thread;
+    
+    private juegoPOO() {
+        setPreferredSize(new Dimension(ANCHO, ALTO));
+
+        ventana = new JFrame(NOMBRE);
+        ventana.setTitle(NOMBRE);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setResizable(false);
+        ventana.setLayout(new BorderLayout());
+        ventana.add(this, BorderLayout.CENTER); //añadir el canvas a la ventana
+        ventana.pack(); // la ventana se autorrellenara con los elementos dentro de ella
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        /*
-        TESTING.. . . . .
-        Holi, porfa cada que necesiten trabajar trabajen
-        sobre sus propias ramas (si no existe, creela).
-        En caso tal de dañar todo en la rama en la que 
-        esten (NUNCA TRABAJEN EN EL MASTER), creen otra
-        y vuelvan a iniciar su trabajo sobre esa rama.
-        En caso tal de terminar de trabajar lo que iban
-        a hacer por favor avisen en el grupo que harán
-        merge. No hagan cambios repentinos sin previo 
-        aviso ya que no se pueden revertir merges.
-        Les deseo un lindo semestre y que salga lo mejor
-        de esto.
-                        Felices Días de CoronaVirus.
-                            Saludos, Luisa.
-                                <3
-        */
+        juegoPOO app = new juegoPOO();
+        app.start();
+    }
+    
+    // con synchronized los dos metodos no seran capaces de ejecutarse al mismo timepo
+    private synchronized void start() {
+        running = true;
+        
+        thread = new Thread(this, "Graficos");
+        thread.start();
+    }
+    
+    private void stop() {
+        running = false;
+    
+        try {
+            thread.join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private void actualizar() {
+        aps++;
+    }
+    
+    private void dibujar() {
+        fps++;
+    }
+
+    
+    // Este metodo sera llamado cada vez que un nuevo hilo sea creado
+    @Override
+    public void run() {
+        while(running) {
+            final int NS_POR_SEGUNDO = 1000000000;
+            final byte APS_OBJETIVO= 60;
+            final double NS_POR_ACTUALIZACION = NS_POR_SEGUNDO / APS_OBJETIVO; // cada cuantos nanosegndos se hara una actualizacion
+            
+            long actualizacionReferencia = System.nanoTime();
+            long contadorReferencia = System.nanoTime();
+            
+            double tiempoTranscurrido;
+            double delta = 0;
+            
+            while(running) {
+                final long inicioBucle = System.nanoTime();
+                
+                tiempoTranscurrido = inicioBucle - actualizacionReferencia;
+                actualizacionReferencia = inicioBucle;
+                
+                delta += tiempoTranscurrido / NS_POR_ACTUALIZACION;
+                
+                while (delta >= 1) {
+                    actualizar();
+                    delta--;
+                }
+                
+                dibujar();
+                
+                if (System.nanoTime() - contadorReferencia > NS_POR_SEGUNDO) {
+                    ventana.setTitle(NOMBRE + " || aps: " + aps + " || FPS: " + fps);
+                    aps = 0;
+                    fps = 0;
+                    contadorReferencia = System.nanoTime();
+                }
+            }
+        }
     }
     
 }
