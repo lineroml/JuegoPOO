fa390937df803b3303428b05b45512accad96eff
Dylan <Dylan@CANTARES>
2020-06-03 14:57:44 -0500

    Silueta Mujer en Ajustes

--- src/principal/Constantes.java
+++ src/principal/Constantes.java
@@ -56,6 +56,8 @@ public class Constantes {
     public static final String BOTONSALIR = "/imagenes/menu/salir.png";
     public static final String BOTONSALIRCONMOUSE = "/imagenes/menu/salirConMouse.png";
     
+    public static final BufferedImage MUJER = CargadorRecursos.cargarImagenCompatibleTranslucida("/imagenes/Mujer.png");
+    
     public final static BufferedImage IMAGENFONDO = CargadorRecursos.cargarImagenCompatibleOpaca("/imagenes/menu/menuInicial.png");
     public final static BufferedImage FONDO = CargadorRecursos.cargarImagenCompatibleOpaca("/imagenes/menu/fondo.png");
     public final static BufferedImage INICIOPARTIDA = CargadorRecursos.cargarImagenCompatibleTranslucida("/imagenes/menu/inicioPartida.png");
