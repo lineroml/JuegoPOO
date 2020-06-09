0dd45dae390a9a35a25af4d3718d7ca75b678536
Luis Evilla <Luis Evilla@DESKTOP-D1CB68G>
2020-06-09 14:11:34 -0500

    ultimsos cambios

--- test/principal/herramientas/CargadorRecursosTest.java
+++ test/principal/herramientas/CargadorRecursosTest.java
@@ -17,24 +17,25 @@ import principal.Constantes;
  * @author Luis Evilla
  */
 public class CargadorRecursosTest {
+
     String separador;
-    
+
     public CargadorRecursosTest() {
         separador = System.getProperty("file.separator");
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -43,44 +44,66 @@ public class CargadorRecursosTest {
      * Test of cargarImagenCompatibleOpaca method, of class CargadorRecursos.
      */
     @Test
-    public void testCargarImagenCompatibleOpaca() {
+    public void testCargarImagenCompatibleOpacaMismaImagen() {
         System.out.println("cargarImagenCompatibleOpaca");
-        
+
         String ruta = separador + "imagenes" + separador + "hojas_Personajes" + separador + "Santana.png";
-        String ruta2 = separador + "imagenes" + separador + "hojas_Personajes" + separador + "SantanaDisparo.png";
-        
+
         BufferedImage expResult = CargadorRecursos.cargarImagenCompatibleOpaca(ruta);
         BufferedImage result = CargadorRecursos.cargarImagenCompatibleOpaca(ruta);
-        
+
         float similitud;
         similitud = ComparadorRecursos.compararImagenes(expResult, result);
         assertEquals("Error, deberia devolver la misma imagen", 100, (int) (similitud));
-    
-        result = CargadorRecursos.cargarImagenCompatibleOpaca(ruta2);
+    }
+
+    @Test
+    public void testCargarImagenCompatibleOpacaDiferenteImagen() {
+        System.out.println("cargarImagenCompatibleOpaca");
+
+        String ruta = separador + "imagenes" + separador + "hojas_Personajes" + separador + "Santana.png";
+        String ruta2 = separador + "imagenes" + separador + "hojas_Personajes" + separador + "SantanaDisparo.png";
+
+        BufferedImage expResult = CargadorRecursos.cargarImagenCompatibleOpaca(ruta);
+        BufferedImage result = CargadorRecursos.cargarImagenCompatibleOpaca(ruta2);
+
+        float similitud;
         similitud = ComparadorRecursos.compararImagenes(result, expResult);
         assertNotEquals("Error, las imagenes deberian ser distintas", 100, (int) (similitud));
     }
 
     /**
-     * Test of cargarImagenCompatibleTranslucida method, of class CargadorRecursos.
+     * Test of cargarImagenCompatibleTranslucida method, of class
+     * CargadorRecursos.
      */
-
     @Test
-    public void testCargarImagenCompatibleTranslucida() {
+    public void testCargarImagenCompatibleTranslucidaDiferenteImagen() {
         System.out.println("cargarImagenCompatibleTranslucida");
         String ruta = Constantes.RUTA_ICONO_RATON;
         String ruta2 = Constantes.RUTA_ICONO_VENTANA;
-        
+
         BufferedImage expResult = CargadorRecursos.cargarImagenCompatibleTranslucida(ruta);
-        BufferedImage result = CargadorRecursos.cargarImagenCompatibleTranslucida(ruta);
-        
+        BufferedImage result = CargadorRecursos.cargarImagenCompatibleTranslucida(ruta2);
+
         float similitud;
-        similitud = ComparadorRecursos.compararImagenes(expResult, result);
-        assertEquals("Error, deberia devolver la misma imagen", 100, (int) (similitud));
-        
-        result = CargadorRecursos.cargarImagenCompatibleOpaca(ruta2);
+
         similitud = ComparadorRecursos.compararImagenes(result, expResult);
         assertNotEquals("Error, las imagenes deberian ser distintas", 100, (int) (similitud));
+    }
+
+    @Test
+    public void testCargarImagenCompatibleTranslucidaMismaImagen() {
+        System.out.println("cargarImagenCompatibleTranslucida");
+        String ruta = Constantes.RUTA_ICONO_RATON;
+        String ruta2 = Constantes.RUTA_ICONO_VENTANA;
+
+        BufferedImage expResult = CargadorRecursos.cargarImagenCompatibleTranslucida(ruta);
+        BufferedImage result = CargadorRecursos.cargarImagenCompatibleTranslucida(ruta);
+
+        float similitud;
+
+        similitud = ComparadorRecursos.compararImagenes(result, expResult);
+        assertEquals("Error, las imagenes deberian ser iguales", 100, (int) (similitud));
     }
 
     /**
@@ -90,7 +113,7 @@ public class CargadorRecursosTest {
     public void testLeerArchivoTexto() {
         System.out.println("leerArchivoTexto");
         String ruta = "/mapas/apocalypse.json";
-        
+
         String expResult = CargadorRecursos.leerArchivoTexto(ruta);
         String result = CargadorRecursos.leerArchivoTexto(ruta);
         assertEquals("Los textos deberian ser iguales", expResult, result);
@@ -103,11 +126,20 @@ public class CargadorRecursosTest {
     public void testCargarFuente() {
         System.out.println("cargarFuente");
         String ruta = "/fuentes/Crumbled-Pixels.ttf";
-        
+
         Font expResult = CargadorRecursos.cargarFuente(ruta);
         Font result = CargadorRecursos.cargarFuente(ruta);
         assertNotNull("Error la fuente no deberia ser nula", result);
-        
+    }
+
+    @Test
+    public void testCargarFuenteMismaFuente() {
+        System.out.println("cargarFuente");
+        String ruta = "/fuentes/Crumbled-Pixels.ttf";
+
+        Font expResult = CargadorRecursos.cargarFuente(ruta);
+        Font result = CargadorRecursos.cargarFuente(ruta);
+
         boolean iguales = ComparadorRecursos.compararFuentes(expResult, result);
         assertTrue("Error las fuentes deberian ser iguales", iguales);
     }
@@ -119,13 +151,35 @@ public class CargadorRecursosTest {
     public void testCargarSonido() {
         System.out.println("cargarSonido");
         String ruta = "/sonidos/Golpe.wav";
-        
+
         Clip expResult = CargadorRecursos.cargarSonido(ruta);
         Clip result = CargadorRecursos.cargarSonido(ruta);
         assertNotNull("Error el audio no deberia ser nulo", result);
-        
+    }
+
+    @Test
+    public void testCargarMismoSonido() {
+        System.out.println("cargarSonido");
+        String ruta = "/sonidos/Golpe.wav";
+
+        Clip expResult = CargadorRecursos.cargarSonido(ruta);
+        Clip result = CargadorRecursos.cargarSonido(ruta);
+
         boolean iguales = ComparadorRecursos.compararAudios(expResult, result);
         assertTrue("Error los audios deberian ser iguales", iguales);
     }
-    
+
+    @Test
+    public void testCargarDiferenteSonido() {
+        System.out.println("cargarSonido");
+        String ruta = "/sonidos/Golpe.wav";
+        String ruta2 = "/sonidos/pelea.wav";
+
+        Clip expResult = CargadorRecursos.cargarSonido(ruta);
+        Clip result = CargadorRecursos.cargarSonido(ruta2);
+
+        boolean iguales = ComparadorRecursos.compararAudios(expResult, result);
+        assertFalse("Error los audios deberian ser iguales", iguales);
+    }
+
 }
